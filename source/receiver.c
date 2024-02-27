43df1ec4689cad58f4fa87064433f0b9da6ead80
Jason Nguyen <khaivnguyen02@gmail.com>
2024-02-27 07:59:30 -0800

    update receiverUDP to signals writer

--- source/receiver.c
+++ source/receiver.c
@@ -1,90 +1,147 @@
+// References: Brian Fraser's Workshops & Beej's Guide to Network Programming 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
 #include <netdb.h>
-#include "receiver.h"
-#include "write.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
 #include "list.h"
+#include "helper.h"
 
-#define DYNAMIC_LEN 128
-#define MSG_MAX_LEN 1024
-
-static pthread_t threadPID;
-//static int socketDescriptor;
-static int myPort;
-static List* list = NULL;
+#include "receiver.h"
+#include "writer.h"
+#include "reader.h"
+#include "sender.h"
 
 
-void* receiveThread(void *arg)
+#define MSG_MAX_LEN 1472
+
+static pthread_t receiverThread;
+
+static List* list;
+static char* myPort;
+
+static int sockfd;
+static struct addrinfo *servinfo;
+
+void* receiveThread(void *unused)
 {
-    List* list = (List*)arg;
-   // Address
-    struct sockaddr_in sin;
-    memset(&sin, 0, sizeof(sin));
-    sin.sin_family = AF_INET;                   // Connection may be from network
-    sin.sin_addr.s_addr = htonl(INADDR_ANY);    // Host to Network long
-    sin.sin_port = htons(myPort);                 // Host to Network short
+    struct addrinfo hints, *p;
+    int getAddrInfoResult;
+    int bindResult;
+    int numberOfBytes;
+    char buffer[MSG_MAX_LEN];
+    char* message;
+    struct sockaddr_in remoteAddr;
+    socklen_t addrLength;
 
-    // Create the socket for UDP
-    int socketDescriptor = socket(PF_INET, SOCK_DGRAM, 0);
-
-    // CHECK ERRORS
-    if (socketDescriptor == -1) {
-        perror("Error creating UDP socket");
-        exit(EXIT_FAILURE);
-    }
-    // Bind the socket to the port (PORT) that we specify
-    bind (socketDescriptor, (struct sockaddr*) &sin, sizeof(sin));
-
-    while(1) {
-        // Get the data (blocking)
-        // Will change sin (the address) to be the address of the client
-        // Note: sin passes information in and out of call!
-        struct sockaddr_in sinRemote;
-        
-        unsigned int sin_len = sizeof(sinRemote);
-        char messageRx[MSG_MAX_LEN];
-        
-        int bytesRx = recvfrom(socketDescriptor,
-            messageRx, MSG_MAX_LEN, 0,
-            (struct sockaddr *) &sinRemote, &sin_len);
-        // Make it null terminated (so string functions work):
-        int terminateIdx = (bytesRx < MSG_MAX_LEN) ? bytesRx : MSG_MAX_LEN - 1;
-        messageRx[terminateIdx] = '\0';
-        List_append(list, messageRx);
-        Write_signalNextChar();
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
 
     
+    getAddrInfoResult = getaddrinfo(NULL, myPort, &hints, &servinfo);
+    if (getAddrInfoResult != 0)
+    {
+        fprintf(stderr, "Receiver: getaddrinfo : %s\n", gai_strerror(getAddrInfoResult));
+        exit(1);
     }
+
+    for (p = servinfo; p != NULL; p = p->ai_next)
+    {
+        sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+        if (sockfd == -1)
+        {
+            perror("Receiver: socket() error");
+            continue;
+        }
+
+        bindResult = bind(sockfd, p->ai_addr, p->ai_addrlen);
+        if (bindResult == -1)
+        {
+            perror("Receiver: bind() error");
+            close(sockfd);
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL)
+    {
+        fprintf(stderr, "Receiver: Failed to bind socket\n");
+        freeaddrinfo(servinfo);
+        exit(2);
+    }
+
+    freeaddrinfo(servinfo);
+
+    while(1)
+    {
+        int i = 0;
+        do
+        {
+            i++;
+            memset(&buffer, 0, MSG_MAX_LEN);
+
+            addrLength = sizeof(remoteAddr);
+            numberOfBytes = recvfrom(sockfd, buffer, MSG_MAX_LEN, 0, (struct sockaddr* ) &remoteAddr, &addrLength);
+        
+            if (numberOfBytes == -1)
+            {
+                perror("Receiver: recvfrom() error\n");
+                exit(1);
+            }
+
+            message = (char*)malloc(sizeof(char)*(numberOfBytes + 1));
+            strncpy(message, buffer, numberOfBytes);
+            message[numberOfBytes] = '\0';
+
+            int addedResult = addMessage(list, message);
+            if (addedResult == -1)
+            {
+                fprintf(stderr, "Receiver: Fail to add message to the list\n");
+            }
+
+            if (!strcmp(message, "!\n") && i == 1)
+            {
+                Writer_signaller();
+                Reader_cancel();
+                Sender_cancel();
+                return NULL;
+            }
+        } while (buffer[numberOfBytes - 1] != '\n' && i != LIST_MAX_NUM_NODES);
+        
+        // Signals writer to write message to screen
+        Writer_signaller();
+    }
+
     return NULL;
 }
 
-void Receiver_init(List* listR, char* myPortR)
+void Receiver_init(List* l, char* myP)
 {
-    if (myPortR == NULL) {
-        fprintf(stderr, "Error: Port number is not provided.\n");
-        return;
-    }
+    list = l;
+    myPort = myP;
 
-    list = listR;
-    myPort = atoi(myPortR);
-
-    pthread_create(
-        &threadPID,         // PID (by pointer)
-        NULL,               // Attributes
-        receiveThread,      // Function
-        (void*)listR);              // Arguments
+    pthread_create(&receiverThread, NULL, receiveThread, NULL);              
 }
 
+void Receiver_cancel()
+{
+    pthread_cancel(receiverThread);
+}
 
 void Receiver_shutdown(void)
 {
-    // Cancel thread
-    pthread_cancel(threadPID);
+    freeaddrinfo(servinfo);
 
-    // Waits for thread to finish
-    pthread_join(threadPID, NULL);
+    close(sockfd);
 
-
+    pthread_join(receiverThread, NULL);
 }
