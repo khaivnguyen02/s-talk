3d53f2c211a2988477afe1838f5095ddfc5f4a19
Jason Nguyen <khaivnguyen02@gmail.com>
2024-02-27 08:06:20 -0800

    update main.c and clean up

--- source/main.c
+++ source/main.c
@@ -1,39 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "receiver.h"
-#include "write.h"
-#include "send.h"
-#include "read.h"
+
 #include "list.h"
 
-//142.58.15.219 
+#include "reader.h"
+#include "writer.h"
+#include "receiver.h"
+#include "sender.h"
 
 int main (int argc, char** argv)
 {
-    List* listReceive = List_create();
-    List* listSend = List_create();
     char* myPort = argv[1];
-    char* connectorHostname = argv[2];
-    char* connectorPort = argv[3];
+    char* remoteMachineName = argv[2];
+    char* remotePort = argv[3];
 
-    // Startup my modules
-       
-    Read_init(listSend);
-    Send_init(listSend, connectorHostname, connectorPort);
-    Receiver_init(listReceive, myPort);
-    Write_init(listReceive);
+    List* list = List_create();
 
-    // Wait for user input
-    char x;
-    scanf("%c", &x);
+    // Initializes modules
+    Reader_init(list);
+    Sender_init(list, remoteMachineName, remotePort);
+    Receiver_init(list, myPort);
+    Writer_init(list);
 
-    // Shutdown my modules
-     
-    Read_shutdown();
-    Send_waitForShutdown();
+    // Shutdown my modules     
+    Reader_shutdown();
+    Sender_shutdown();
     Receiver_shutdown();
-    Write_waitForShutdown();
-   
-    printf("Done\n");
+    Writer_shutdown();
+
+    List_free(list, free);
+
     return 0;
 }
